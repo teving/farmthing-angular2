9fc902148aaa5e0c363f673e74a28987017684ae
Kevin Gurton <kevin.gurton@gmail.com>
2017-01-24 17:59:21 +0000

    Moved out webpack dev code into its own file

---
+++ config/webpack.common.js
@@ -0,0 +1,36 @@
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const helpers = require('./helpers');
+
+module.exports = {
+   entry: {
+      polyfills: './src/polyfills.ts',
+      vendor: './src/vendor.ts',
+      app: './src/main.ts'
+   },
+   resolve: {
+      extensions: ['.ts', '.js']
+   },
+   module: {
+      rules: [
+         {
+            test: /\.ts$/,
+            use: ['awesome-typescript-loader', 'angular2-template-loader']
+         }
+      ]
+   },
+   plugins: [
+      new CommonsChunkPlugin({
+         name: ['app', 'vendor', 'polyfills']
+      }),
+      new HtmlWebpackPlugin({
+         template: 'src/index.html'
+      }),
+      new ContextReplacementPlugin(
+        /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+        helpers.root('src')
+      )
+   ]
+};
