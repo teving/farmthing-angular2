7674e451555e7efe2e136e3f47e99162f28c1cfb
Kevin Gurton <kevin.gurton@gmail.com>
2017-03-13 16:53:29 +0000

    Added location model

---
+++ src/app/core/models/location.model.ts
@@ -0,0 +1,11 @@
+interface ILocation {
+   latitude: Number;
+   longitude: Number;
+}
+
+export function location(latitude: Number, longitude: Number): ILocation {
+   return {
+      latitude,
+      longitude
+   };
+}
