246242d1f2cc10396cbfa0e3f66e8f57640634d4
Kevin Gurton <kevin.gurton@gmail.com>
2017-03-16 14:44:39 +0000

    Added bare google-map component

--- src/app/buyers/buyers.module.ts
+++ src/app/buyers/buyers.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
+import { GoogleMapComponent } from './google-map/google-map.component';
+
 @NgModule({
-   imports: [ SharedModule ]
+   imports: [ SharedModule ],
+   declarations: [ GoogleMapComponent ],
+   exports: [ GoogleMapComponent ]
 })
 export class BuyersModule {}
---
+++ src/app/buyers/google-map/google-map.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'urm-google-map',
+   template: '<div></div>'
+})
+export class GoogleMapComponent {}
