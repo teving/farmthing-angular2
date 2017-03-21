0aafcc13e379fb4ff750ae3b2c619f99e25b267e
Kevin Gurton <kevin.gurton@gmail.com>
2017-03-21 20:21:45 +0000

    Enabled sourcemap debugging with webpack

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,7 +3,7 @@ const commonConfig = require('./webpack.common.js');
 const helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-   devtool: 'cheap-module-eval-source-map',
+   devtool: 'inline-source-map',
    output: {
       path: helpers.root('dist'),
       publicPath: 'http://localhost:3000',
@@ -13,11 +13,24 @@ module.exports = webpackMerge(commonConfig, {
    module: {
       rules: [
          {
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'source-map-loader'
+         },
+         {
+            enforce: 'pre',
+            test: /\.ts$/,
+            loader: 'source-map-loader'
+         },
+         {
             test: /\.css$/,
             use: ['style-loader', 'css-loader'],
             include: [helpers.root('src', 'styles')]
          }
       ]
+   },
+   resolve: {
+      extensions: ['.ts', '.js']
    },
    devServer: {
       historyApiFallback: true,
