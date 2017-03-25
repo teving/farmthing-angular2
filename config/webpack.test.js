0a0a3f49eda1028a1a9d6c570aa266097ddbe0ed
Kevin Gurton <kevin.gurton@gmail.com>
2017-03-25 21:57:24 +0000

    Updated webpack test config to work with Angular 4

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -47,7 +47,7 @@ module.exports = {
    },
    plugins: [
       new ContextReplacementPlugin(
-         /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+         /angular(\\|\/)core(\\|\/)@angular/,
          helpers.root('src')
       )
    ]
