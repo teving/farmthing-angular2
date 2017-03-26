bb61e19fbdb0de80edd52881ad10e77a4f510b2d
Kevin Gurton <kevin.gurton@gmail.com>
2017-03-26 13:35:41 +0100

    Added GoogleMapService to buyers module providers

--- src/app/buyers/buyers.module.ts
+++ src/app/buyers/buyers.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { GoogleMapComponent } from './google-map/google-map.component';
+import { GoogleMapService } from './shared/google-map.service';
 
 @NgModule({
    imports: [ SharedModule ],
    declarations: [ GoogleMapComponent ],
-   exports: [ GoogleMapComponent ]
+   exports: [ GoogleMapComponent ],
+   providers: [ GoogleMapService ]
 })
 export class BuyersModule {}
