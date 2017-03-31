c43a439153373d69aba8c424cc459b12793474ca
Kevin Gurton <kevin.gurton@gmail.com>
2017-03-31 16:21:51 +0100

    Removed google-map-service from Buyers module

--- src/app/buyers/buyers.module.ts
+++ src/app/buyers/buyers.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { GoogleMapComponent } from './google-map/google-map.component';
-import { GoogleMapService } from './shared/google-map.service';
 
 @NgModule({
    imports: [ SharedModule ],
    declarations: [ GoogleMapComponent ],
-   exports: [ GoogleMapComponent ],
-   providers: [ GoogleMapService ]
+   exports: [ GoogleMapComponent ]
 })
 export class BuyersModule {}
